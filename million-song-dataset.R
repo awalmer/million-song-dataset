f526cb69405b2ac1512f960ea530fb50e41c2e01
Auralee Walmer <auraleewalmer@Auralees-MacBook-Pro.local>
2020-11-10 00:29:45 -0800

    viz and word freq

--- million-song-dataset.R
+++ million-song-dataset.R
@@ -14,6 +14,7 @@ library(leaflet)
 library(ggplot2)
 library(htmlwidgets)
 library(plotly)
+library(stringr)
 
 
 #-------------#
@@ -26,35 +27,41 @@ data <- read.csv("msd-cleaned-subset.csv")
 # CLEANING
 #-------------#
 
+subset <- data[data$year>= 1990 & data$year <= 2010 & data$segments_loudness_max<6000,]
+
 
 #----------------#
 # VISUALIZATIONS
 #----------------#
 
-data$geohash <- gh_encode(data$artist_latitude, data$artist_longitude, precision = 8)
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(data$geohash, popup="The birthplace of R")
-m  # Print the map
-
-
-timeseries <- ggplot(data, aes(x = `year`, y = `segments_loudness_max`, color = time_signature,
-                                                   text = paste('Song Title:', `title`,
-                                                                '<br>Artist Name:', `artist_name`
-                                                                )
-                                        )) + 
+timeseries <- ggplot(data, aes(x = `year`, y = `segments_loudness_max`, color = time_signature)) + 
   geom_point(alpha = 0.85, shape=16) +
-  stat_smooth(method='lm', formula= y~x) + 
+  geom_smooth(method = 'lm', aes(x = `year`, y = `segments_loudness_max`)) + 
   theme(legend.key.height = unit(1,"cm"), legend.key.width = unit(.2, "cm")) +
   xlab("Year") + ylab("Maximum dB Reached in Song") +
   labs(title = "Are Songs Getting Louder Over Time?",
        caption = "Million Song Dataset: Subset of 10,000") +
-  scale_x_continuous(breaks = seq(1960,2010,5), limits = c(1960,2010))
-  #scale_y_date(labels = date_format("%e %b %Y"), breaks = date_breaks("6 weeks"))
+  scale_x_continuous(breaks = seq(1960,2010,5), limits = c(1960,2010)) +
+  stat_smooth(method = "lm", col = "red")
+
 saveWidget(ggplotly(timeseries, tooltip = "text"), file = "time series test.html")
 
+## doesn't seem like songs are getting longer over time or louder over time. :-/
+ggplot(subset, aes(x = `year`, y = `segments_loudness_max`, color = time_signature)) + geom_point() + stat_smooth(method = 'lm')
+ggplot(subset, aes(x = `year`, y = `duration`, color = segments_loudness_max)) + geom_point() + stat_smooth(method = 'lm', color="red")
+
+# linear regression:
+linearMod <- lm(`segments_loudness_max` ~ `year`, data=data)
 
 
-# most popular time signature by year?
-# 
+## would it be interesting to look at all the individual words in song titles, then rank the most common ones?
+str_split(data$release[1], " ", simplify = TRUE)
+vect <- c()
+for (n in 1:nrow(data)) {
+  vals <- as.vector(str_split(data$release[n], " ", simplify = TRUE))
+  vect <- append(vals, vect)
+}
+# vect = individual words
+
+
 
